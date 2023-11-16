b34f58f5573d863bcd1fb7ec67bd0a139d4879e6
amsesk <amsesk@umich.edu>
2023-11-16 21:31:23 +0000

    apply pvalue cutoff and change color scheme

--- scripts/nk_rank_bar.R
+++ scripts/nk_rank_bar.R
@@ -1,10 +1,30 @@
-
 library(ggplot2)
+source("figures/genericFigureSettings.R")
+source("scripts/parse_comparisons.R")
+
+pValSymnum <- function(x, showNs = TRUE) {
+  tmp <- sapply(x, function(y) {
+                  if (is.na(y)) {
+                    return(NA)
+                  }
+
+                  if (y < 0.001) {
+                    return("***")
+                  } else if (y >= 0.001 & y < 0.01) {
+                    return("**")
+                  } else if (y >= 0.01 & y < 0.05) {
+                    return("*")
+                  } else {
+                    return(ifelse(showNs, "ns", ""))
+                  }
+})
+
+  return(tmp)
+}
 
 so_pln_only = readRDS("rds/so_pln_only.rds")
 
-
-clusters = unique(so[["manualAnnot"]])
+clusters = unique(so_pln_only[["manualAnnot"]])
 bcell_clusters = clusters[which(startsWith(clusters[,1], "B")),1]
 nk_clusters = c("NK", "NK/ILC")
 
@@ -18,21 +38,24 @@ colnames(so_pln_only[[]])
 sig_genes = readRDS("rds/wuv_compres_rna_genelist_V1.rds")
 goirb = read.table("NK_list_231025.csv", sep=",", header=TRUE)
 goirb = goirb[goirb$group == "rank",]$gene
-sig_genes=sig_genes[sig_genes$gene %in% goirb & sig_genes$matchup == "T1D_vs_ND", & sig_genes$p_val_adj_all >= 5]
+colnames(sig_genes)
+sig_genes=sig_genes[sig_genes$gene %in% goirb & sig_genes$matchup == "T1D_vs_ND" & sig_genes$p_val_adj_all <= 0.05,]
 dim(sig_genes)
 
 colnames(sig_genes)
-sig_genes
-colscale = hcl.colors(n=21, palette = "Earth")[c(1,6,11,16,21)]
+
 sig_genes$gene = factor(sig_genes$gene, levels=sig_genes$gene[order(sig_genes$avg_log2FC, decreasing=FALSE)])
-pdf("/srv/http/betts/hpap/figures/nk_ranked_bar_e-10.pdf", width=8.5, height=16.5)
-ggplot(data=sig_genes, aes(x=gene, y=avg_log2FC, fill=-log10(p_val_adj_all))) +
+sig_genes$pvalsymm = pValSymnum(sig_genes$p_val_adj_all)
+
+pdf("/srv/http/betts/hpap/figures/nk_ranked_bar.pdf", width=8.5, height=8.5)
+ggplot(data=sig_genes, aes(x=gene, y=avg_log2FC, fill=pvalsymm)) +
   geom_bar(stat="identity", color = "black") +
-  scale_fill_gradient2(low = colscale[1], mid = colscale[3], high = colscale[5], midpoint = 11) +
+  scale_fill_manual(values = COLORS[["pval-heatmap"]]) +
   geom_text(data = subset(sig_genes, avg_log2FC >= 0), aes(label=gene, x=gene), y=0.009, size = 2.65, hjust=0)+
   geom_text(data = subset(sig_genes, avg_log2FC < 0), aes(label=gene, x=gene), y=-0.015, size = 2.65, hjust=1)+
   geom_hline(yintercept = 0, color = "black") +
   coord_flip()+
+  ylab("log2(Fold Change)")+
   theme_bw() +
   theme(
     panel.grid = element_blank(),
@@ -41,6 +64,7 @@ ggplot(data=sig_genes, aes(x=gene, y=avg_log2FC, fill=-log10(p_val_adj_all))) +
     panel.border = element_blank(),
     axis.ticks.y = element_blank(),
     axis.ticks.length.y = unit(0,"cm"),
+    legend.title = element_blank(),
     axis.line.x = element_line(color="black")
   )
 dev.off()
